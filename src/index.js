12f98ee74a71b4f7feae7f2285b1b4e5d5bfaf28
satyakabiroffical <pranjay@satyakabir.co.in>
2026-03-15 12:38:30 +0530

    file structure fix + util code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv";
-import connectDB from "../db/index.js";
+import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB()
+connectDB().then(
+
+    app.on("error", (err)=>{
+        console.log("Error", err);
+        throw err;
+    }),
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at ${process.env.PORT}`);
+
+    })
+).catch(
+    (err)=>{
+        console.log("MongoDB connection faileld",err);
+    }
+);
